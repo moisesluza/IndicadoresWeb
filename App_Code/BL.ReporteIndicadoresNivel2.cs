c63da3a22ca2c514a9f87a187e62dc6b9a6456cb
Moises Luza <moises.luza@gmail.com>
2012-03-23 18:03:09 -0500

    Se corrige el error de los totales de tickets y se agrega un mtodo para corregir los tickets mal registrados

--- App_Code/BL.ReporteIndicadoresNivel2.cs
+++ App_Code/BL.ReporteIndicadoresNivel2.cs
@@ -19,7 +19,7 @@ namespace BL
     public class ReporteIndicadoresNivel2
     {
         private static ReporteIndicadoresNivel2 objRpt = null;
-        private DataTable dt = null;
+        private DataTable dtNivel2 = null;
 
         private ReporteIndicadoresNivel2()
         {
@@ -38,7 +38,7 @@ namespace BL
         public DataTable ObtenerRptTiempoRespuestaOP()
         {
             //Se filtran las sedes
-            DataTable dtFiltrada=DataHelper.Filter(dt, "Tipo_Sede_Usuario='OP' and Estado in ('Asignado','Registrado') and Prioridad <> 'Ninguno'");
+            DataTable dtFiltrada=DataHelper.Filter(dtNivel2, "Tipo_Sede_Usuario='OP' and Estado in ('Asignado','Registrado') and Prioridad <> 'Ninguno'");
 
             //Se agrupan los tiempos de los estados Asignado y en proceso
             DataTable dtAgrupadaEstados = DataHelper.Distinct(dtFiltrada, new String[] { "obj_id", "Prioridad", "Tiempo_Minimo" }, "obj_id");
@@ -62,7 +62,7 @@ namespace BL
             AgregarTotales(ref dtAgrupada);
 
             //Se calculan los totales
-            CalcularTotalesPorPrioridad(ref dtAgrupada, dtAgrupadaEstados);
+            CalcularTotalTicketsPorPrioridad(ref dtAgrupada, dtAgrupadaEstados);
 
             dtAgrupada.TableName = "TiempoRespuestaOP";
             return dtAgrupada; 
@@ -71,7 +71,7 @@ namespace BL
         public DataTable ObtenerRptTiempoSolucionOP()
         {
             //Se filtran las sedes
-            DataTable dtFiltrada = DataHelper.Filter(dt, "Tipo_Sede_Usuario='OP' and Estado='En proceso' and Prioridad <> 'Ninguno'");
+            DataTable dtFiltrada = DataHelper.Filter(dtNivel2, "Tipo_Sede_Usuario='OP' and Estado='En proceso' and Prioridad <> 'Ninguno'");
 
             //Se obtienen los valores de prioridad agrupados
             DataTable dtAgrupada = DataHelper.Distinct(dtFiltrada, new String[] { "Prioridad", "Tiempo_Minimo" }, "Prioridad");
@@ -80,7 +80,7 @@ namespace BL
             AgregarTotales(ref dtAgrupada);
 
             //Se calculan los totales
-            CalcularTotalesPorPrioridad(ref dtAgrupada, dtFiltrada);
+            CalcularTotalTicketsPorPrioridad(ref dtAgrupada, dtFiltrada);
 
             dtAgrupada.TableName = "TiempoSolucionOP";
             return dtAgrupada;
@@ -89,7 +89,7 @@ namespace BL
         public DataTable ObtenerRptTiempoRespuestaODyOR()
         {
             //Se filtran las sedes
-            DataTable dtFiltrada = DataHelper.Filter(dt, "Tipo_Sede_Usuario in ('OD','OR') and Estado in ('Asignado','Registrado') and Prioridad <> 'Ninguno'");
+            DataTable dtFiltrada = DataHelper.Filter(dtNivel2, "Tipo_Sede_Usuario in ('OD','OR') and Estado in ('Asignado','Registrado') and Prioridad <> 'Ninguno'");
             
             //Se agrupan los tiempos de los estados Asignado y en proceso
             DataTable dtAgrupadaEstados = DataHelper.Distinct(dtFiltrada, new String[] { "obj_id", "Tipo_Sede_Usuario", "Tiempo_Minimo" }, "obj_id");
@@ -122,7 +122,7 @@ namespace BL
         public DataTable ObtenerRptTiempoSolucionODyOR()
         {
             //Se filtran las sedes
-            DataTable dtFiltrada = DataHelper.Filter(dt, "Tipo_Sede_Usuario in ('OD','OR') and Estado='En proceso' and Prioridad <> 'Ninguno'");
+            DataTable dtFiltrada = DataHelper.Filter(dtNivel2, "Tipo_Sede_Usuario in ('OD','OR') and Estado='En proceso' and Prioridad <> 'Ninguno'");
 
             //Se obtienen los valores de prioridad agrupados
             DataTable dtAgrupada = DataHelper.Distinct(dtFiltrada, new String[] { "Tipo_Sede_Usuario", "Tiempo_Minimo" }, "Tipo_Sede_Usuario");
@@ -135,44 +135,6 @@ namespace BL
 
             dtAgrupada.TableName = "TiempoSolucionODyOR";
             return dtAgrupada;
-        }
-                
-        private void AgregarTotales(ref DataTable i_dtOrigen)
-        {
-            i_dtOrigen.Columns.Add("SLA", typeof(int));
-            i_dtOrigen.Columns.Add("Total_Tickets", typeof(int));
-            i_dtOrigen.Columns.Add("Cumple_SLA", typeof(int));
-            i_dtOrigen.Columns.Add("No_Cumple_SLA", typeof(int));
-            i_dtOrigen.Columns.Add("Porcentaje", typeof(int));
-        }
-
-        private void CalcularTotalesPorPrioridad(ref DataTable i_dtAgrupada, DataTable i_dtDatos)
-        {
-            int iSla = 0;
-            int.TryParse(ConfigurationSettings.AppSettings["SLA_TIEMPO_RESPUESTA_SOLUCION"],out iSla);
-            for (int i = 0; i < i_dtAgrupada.Rows.Count; i++)
-            {
-                i_dtAgrupada.Rows[i]["SLA"] = iSla;
-                i_dtAgrupada.Rows[i]["Total_Tickets"] = i_dtDatos.Compute("Count(tiempo)", "Prioridad" + " = '" + i_dtAgrupada.Rows[i]["Prioridad"] + "'");
-                i_dtAgrupada.Rows[i]["Cumple_SLA"] = i_dtDatos.Compute("Count(tiempo)", "Prioridad" + " = '" + i_dtAgrupada.Rows[i]["Prioridad"] + "' and tiempo <= " + i_dtAgrupada.Rows[i]["Tiempo_Minimo"] + "");
-                i_dtAgrupada.Rows[i]["No_Cumple_SLA"] = i_dtDatos.Compute("Count(tiempo)", "Prioridad" + " = '" + i_dtAgrupada.Rows[i]["Prioridad"] + "' and tiempo > " + i_dtAgrupada.Rows[i]["Tiempo_Minimo"] + "");
-                i_dtAgrupada.Rows[i]["Porcentaje"] = (double.Parse(i_dtAgrupada.Rows[i]["Cumple_SLA"].ToString()) / double.Parse(i_dtAgrupada.Rows[i]["Total_Tickets"].ToString())) * 100;
-            }
-        }
-
-        private void CalcularTotalesPorTipoSede(ref DataTable i_dtAgrupada, DataTable i_dtDatos)
-        {
-            int iSla = 0;
-            int.TryParse(ConfigurationSettings.AppSettings["SLA_TIEMPO_RESPUESTA_SOLUCION"],out iSla);
-            for (int i = 0; i < i_dtAgrupada.Rows.Count; i++)
-            {
-                i_dtAgrupada.Rows[i]["SLA"] = iSla;
-                i_dtAgrupada.Rows[i]["Total_Tickets"] = i_dtDatos.Compute("Count(tiempo)", "Tipo_Sede_Usuario" + " = '" + i_dtAgrupada.Rows[i]["Tipo_Sede_Usuario"] + "'");
-                i_dtAgrupada.Rows[i]["Cumple_SLA"] = i_dtDatos.Compute("Count(tiempo)", "Tipo_Sede_Usuario" + " = '" + i_dtAgrupada.Rows[i]["Tipo_Sede_Usuario"] + "' and tiempo <= " + i_dtAgrupada.Rows[i]["Tiempo_Minimo"] + "");
-                i_dtAgrupada.Rows[i]["No_Cumple_SLA"] = i_dtDatos.Compute("Count(tiempo)", "Tipo_Sede_Usuario" + " = '" + i_dtAgrupada.Rows[i]["Tipo_Sede_Usuario"] + "' and tiempo > " + i_dtAgrupada.Rows[i]["Tiempo_Minimo"] + "");
-                i_dtAgrupada.Rows[i]["Porcentaje"] = (double.Parse(i_dtAgrupada.Rows[i]["Cumple_SLA"].ToString()) / double.Parse(i_dtAgrupada.Rows[i]["Total_Tickets"].ToString())) * 100;
-            }
-
         }
 
         public void obtenerTiemposPorEstado()
@@ -197,7 +159,11 @@ namespace BL
 
             try
             {
-                dt = objTpE.Listar_TiemposPorEstado(dtFecIni, dtFecFin, lsEstado, lsGrupo);
+                dtNivel2 = objTpE.Listar_TiemposPorEstado(dtFecIni, dtFecFin, lsEstado, lsGrupo);
+                //Comentado ya que no se ha informado si es que el error de los tickets mal registrados se solucionará 
+                //en el reporte o en la información del service desk
+                //CompletarTiempoSolucion();
+                
                 //dt = objTpE.Listar_TiemposPorEstado(new DateTime(DateTime.Today.Year, 2, 1), new DateTime(DateTime.Today.Year, 2, 29, 23, 59, 59), lsEstado, lsGrupo);
             }
             catch (SqlException ex)
@@ -208,10 +174,81 @@ namespace BL
             {
                 throw new Exception("Ocurrió un error no controlado cuando se intento obtener los tiempos por estado.", ex);
             }
-            
+
+        }
+        
+        private void AgregarTotales(ref DataTable i_dtOrigen)
+        {
+            i_dtOrigen.Columns.Add("SLA", typeof(int));
+            i_dtOrigen.Columns.Add("Total_Tickets", typeof(int));
+            i_dtOrigen.Columns.Add("Cumple_SLA", typeof(int));
+            i_dtOrigen.Columns.Add("No_Cumple_SLA", typeof(int));
+            i_dtOrigen.Columns.Add("Porcentaje", typeof(int));
+        }
+
+        private void CalcularTotalTicketsPorPrioridad(ref DataTable i_dtAgrupada, DataTable i_dtDatos)
+        {
+            int iSla = 0;
+
+            int.TryParse(ConfigurationManager.AppSettings["SLA_TIEMPO_RESPUESTA_SOLUCION"], out iSla);
+            for (int i = 0; i < i_dtAgrupada.Rows.Count; i++)
+            {
+                i_dtAgrupada.Rows[i]["SLA"] = iSla;
+                i_dtAgrupada.Rows[i]["Total_Tickets"] = CalcularTotalTicketsPorPrioridadOP(Convert.ToInt32(i_dtAgrupada.Rows[i]["Prioridad"]));
+                i_dtAgrupada.Rows[i]["Cumple_SLA"] = i_dtDatos.Compute("Count(tiempo)", "Prioridad" + " = '" + i_dtAgrupada.Rows[i]["Prioridad"] + "' and tiempo <= " + i_dtAgrupada.Rows[i]["Tiempo_Minimo"] + "");
+                i_dtAgrupada.Rows[i]["No_Cumple_SLA"] = i_dtDatos.Compute("Count(tiempo)", "Prioridad" + " = '" + i_dtAgrupada.Rows[i]["Prioridad"] + "' and tiempo > " + i_dtAgrupada.Rows[i]["Tiempo_Minimo"] + "");
+                i_dtAgrupada.Rows[i]["Porcentaje"] = (double.Parse(i_dtAgrupada.Rows[i]["Cumple_SLA"].ToString()) / double.Parse(i_dtAgrupada.Rows[i]["Total_Tickets"].ToString())) * 100;
+            }
+        }
+
+        private void CalcularTotalesPorTipoSede(ref DataTable i_dtAgrupada, DataTable i_dtDatos)
+        {
+            int iSla = 0;
+            int.TryParse(ConfigurationSettings.AppSettings["SLA_TIEMPO_RESPUESTA_SOLUCION"],out iSla);
+            for (int i = 0; i < i_dtAgrupada.Rows.Count; i++)
+            {
+                i_dtAgrupada.Rows[i]["SLA"] = iSla;
+                i_dtAgrupada.Rows[i]["Total_Tickets"] = i_dtDatos.Compute("Count(tiempo)", "Tipo_Sede_Usuario" + " = '" + i_dtAgrupada.Rows[i]["Tipo_Sede_Usuario"] + "'");
+                i_dtAgrupada.Rows[i]["Cumple_SLA"] = i_dtDatos.Compute("Count(tiempo)", "Tipo_Sede_Usuario" + " = '" + i_dtAgrupada.Rows[i]["Tipo_Sede_Usuario"] + "' and tiempo <= " + i_dtAgrupada.Rows[i]["Tiempo_Minimo"] + "");
+                i_dtAgrupada.Rows[i]["No_Cumple_SLA"] = i_dtDatos.Compute("Count(tiempo)", "Tipo_Sede_Usuario" + " = '" + i_dtAgrupada.Rows[i]["Tipo_Sede_Usuario"] + "' and tiempo > " + i_dtAgrupada.Rows[i]["Tiempo_Minimo"] + "");
+                i_dtAgrupada.Rows[i]["Porcentaje"] = (double.Parse(i_dtAgrupada.Rows[i]["Cumple_SLA"].ToString()) / double.Parse(i_dtAgrupada.Rows[i]["Total_Tickets"].ToString())) * 100;
+            }
+
+        }
+
+        private int CalcularTotalTicketsPorPrioridadOP(int i_iPriotidad)
+        {
+            //Se filtran las sedes
+            DataTable dtFiltrada = DataHelper.Filter(dtNivel2, "Tipo_Sede_Usuario='OP' and Prioridad = '" + i_iPriotidad.ToString() + "'");
+
+            //Se hace disticnt de los tickets
+            DataTable dtTickets = DataHelper.Distinct(dtFiltrada, new String[] { "obj_id" }, "Prioridad");
+
+            return dtTickets.Rows.Count;
+        }
+
+        /// <summary>
+        /// Se encarga de completar los tiempos En Proceso para los tickets mal registrados 
+        /// (No debe existir tickets de nivel 2 sin tiempos En Proceso)
+        /// </summary>
+        private void CompletarTiempoSolucion()
+        {
+            DataTable dtTickets = DataHelper.Distinct(dtNivel2, new String[] { "obj_id" }, "Prioridad");
+            foreach (DataRow dr in dtTickets.Rows)
+            {
+                //Si el ticket no tiene tiempo "En Proceso", se crea el registro con tiempo cero
+                if (dtNivel2.Select("obj_id=" + dr["obj_id"] + " and Estado = 'En Proceso'").Length == 0)
+                {
+                    dtNivel2.ImportRow(dr);
+                    dtNivel2.Rows[dtNivel2.Rows.Count - 1]["Estado"] = "En Proceso";
+                    dtNivel2.Rows[dtNivel2.Rows.Count - 1]["tiempo"] = 0;
+                }
+            }
+            dtNivel2.AcceptChanges();
         }
     }
 
     
 }
+
 
